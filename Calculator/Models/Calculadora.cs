bfeb67106910ac7ea522bb09bee84d25295266cc
kevinbh168 <kevin.brena@tecsup.edu.pe>
2019-10-28 15:25:38 -0500

    avance commit

---
+++ Calculator/Models/Calculadora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Models
+{
+    class Calculadora
+    {
+        public int currentState { get; set; }
+        public string mathOperator { get; set; }
+        public double firstNumber { get; set; }
+        public double secondNumber { get; set; }
+
+    }
+}
